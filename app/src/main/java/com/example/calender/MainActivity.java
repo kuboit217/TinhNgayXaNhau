13d2d4d4aff9a01f84db7aeb969bf5c6cc6bf369
vietdinh <Hoangviet@217>
2020-04-25 22:52:58 +0700

    cập nhật mainactivity

--- app/src/main/java/com/example/calender/MainActivity.java
+++ app/src/main/java/com/example/calender/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.calender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtDateOne, edtDateTwo;
+    Button btnTinhNgay;
+    TextView txtNgayXa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
